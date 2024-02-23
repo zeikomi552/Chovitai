e73b5f30778dc467c364ed3487ce43da7b251524
zeikomi552 <premium.tsubu@gmail.com>
2024-02-24 05:51:17 +0900

    A1111 設定画面を作成

---
+++ Chovitai/ViewModels/A1111SettingVM.cs
@@ -0,0 +1,42 @@
+﻿using MVVMCore.BaseClass;
+using MVVMCore.Common.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chovitai.ViewModels
+{
+    public class A1111SettingVM : ViewModelBase
+    {
+        #region 画面初期化処理
+        /// <summary>
+        /// 画面初期化処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public override void Init(object sender, EventArgs e)
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                ShowMessage.ShowErrorOK(ex.Message, "Error");
+            }
+        }
+        #endregion
+        #region クローズ処理
+        /// <summary>
+        /// クローズ処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public override void Close(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
