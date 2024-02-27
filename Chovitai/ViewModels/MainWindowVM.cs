1e97302b5e3e8f8649b021d54fb9ab81afd2f79a
zeikomi552 <premium.tsubu@gmail.com>
2024-02-28 05:55:35 +0900

    デバッグフラグを追加（Prompt画面は実装中なので非表示）

--- Chovitai/ViewModels/MainWindowVM.cs
+++ Chovitai/ViewModels/MainWindowVM.cs
@@ -1,4 +1,5 @@
-﻿using MVVMCore.BaseClass;
+﻿using Chovitai.Common;
+using MVVMCore.BaseClass;
 using MVVMCore.Common.Utilities;
 using System;
 
@@ -6,6 +7,24 @@ namespace Chovitai.ViewModels
 {
     public class MainWindowVM : ViewModelBase
     {
+        #region デバッグフラグ[DebugF]プロパティ
+        /// <summary>
+        /// デバッグフラグ[DebugF]プロパティ用変数
+        /// </summary>
+        bool _DebugF = false;
+        /// <summary>
+        /// デバッグフラグ[DebugF]プロパティ
+        /// </summary>
+        public bool DebugF
+        {
+            get
+            {
+                return GblValues.Instance.Config!.Item.DebugF;
+            }
+        }
+        #endregion
+
+
         #region 画面初期化処理
         /// <summary>
         /// 画面初期化処理
